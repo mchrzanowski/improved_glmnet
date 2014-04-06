ee403b29433bc0539f71823691eee8f4f290aa48
Mike Chrzanowski <mike.chrzanowski0@gmail.com>
2014-04-06 02:56:38 -0700

    examples and makefile

---
+++ examples/basic_armadillo.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <armadillo>
+ 
+using namespace std;
+using namespace arma;
+ 
+int main()
+{
+  
+  wall_clock timer;
+  size_t size = 10000;
+  mat A = randn(size, size);
+  mat B = randn(size, size);
+
+  timer.tic();
+  mat C = A * B;
+  double n_secs = timer.toc();
+
+  cout << accu(C) << endl;
+  cout << "Took: " << n_secs << " seconds." << endl;
+
+  return 0;
+}
