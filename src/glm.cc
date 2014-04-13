d45f8e65f29eccc469deb27932c1b839155e315b
Mike Chrzanowski <mike.chrzanowski0@gmail.com>
2014-04-13 02:27:33 -0700

    change.

--- src/glm.cc
+++ src/glm.cc
@@ -8,7 +8,7 @@ GLM* GLM::makeGLM(const mat &X, const vec &y,
         const double lambda, const double eta){
     const uword m = X.n_rows;
     const uword n = X.n_cols;
-    if (n > 3.5 * m){   // this seems to work best.
+    if (n >= 3 * m){   // this seems to work best.
         std::cout << "Created FatGLM class" << std::endl;
         return new FatGLM(X, y, lambda, eta);
     }
