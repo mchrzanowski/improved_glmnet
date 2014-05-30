1216e9e47c60b0ac1a8248319fc3c718fae2d571
Mike Chrzanowski <mike.chrzanowski0@gmail.com>
2014-05-30 00:46:02 -0700

    change calling procedure for CVX validation

--- src/cvx_validation.cc
+++ src/cvx_validation.cc
@@ -7,47 +7,11 @@ using namespace std;
 using namespace arma;
 
 static void run(GLM *g, const mat &A, const colvec &b, colvec &z,
-                double lambda, double eta){
-  
-  wall_clock timer;
-  timer.tic();
-  g->solve(z, lambda);
-  double time = timer.toc();
-  double error = GLM::evaluate(A, b, z, lambda, eta);
+                double eta){
 
-  cout << "Lambda: " << lambda << endl;
-  cout << "Error: " << error << endl;
-  cout << "Runtime: " << time << " seconds. " << endl;
-}
-
-/*
-CVX  verifier.
-Will solve the elastic net optimization problem for a given A, b, z, eta, and
-lambda value. You should then solve the problem in CVX and make sure the
-optimal values are close to each other.
-argc is expected to be 7.
-argv[1] = lambda
-argv[2] = eta
-argv[3] = location of A matrix
-argv[4] = location of b vector
-argv[5] = location of initial z vector
-argv[6] = use the unoptimized solver?
-*/
-int main(int argc, char **argv){
-
-  assert(argc == 7);
-  arma_rng::set_seed(0);
-
-  double lambda = strtod(argv[1], NULL);
-  double eta = strtod(argv[2], NULL);
-  bool use_stupid_solver = strtol(argv[6], NULL, 10) > 0;
-
-  mat A; colvec b; colvec z;
-  A.load(argv[3], csv_ascii);
-  b.load(argv[4], csv_ascii);
-  z.load(argv[5], csv_ascii);
-
-  lambda = 0.5 * norm(A.t() * b, "inf") / eta;
+  // pick a reasonable lambda, defined
+  // as half of the lambda value for which z^\star ~ 0.
+  double lambda = 0.25 * g->maxLambda();
 
   cout << A.n_rows << "x" << A.n_cols 
     << "\t" << b.n_rows << "\t" 
@@ -56,9 +20,46 @@ int main(int argc, char **argv){
   cout << "Lambda: " << lambda << endl;
   cout << "Eta: " << eta << endl;
 
+  wall_clock timer;
+  timer.tic();
+  g->solve(z, lambda);
+  double time = timer.toc();
+  double error = GLM::evaluate(A, b, z, lambda, eta);
+
+  cout << "Error: " << error << endl;
+  cout << "Runtime: " << time << " seconds. " << endl;
+}
+
+/*
+CVX  verifier.
+Will solve the elastic net optimization problem for a given A, b, z, and eta.
+You should then solve the problem in CVX and make sure the
+optimal values are close to each other.
+As you probably don't know a good lambda value, we'll choose one for you.
+Just plug in the value we report into CVX.
+
+argc is expected to be 6.
+argv[1] = eta
+argv[2] = location of A matrix
+argv[3] = location of b vector
+argv[4] = location of initial z vector
+argv[5] = use the unoptimized solver?
+*/
+int main(int argc, char **argv){
+
+  assert(argc == 6);
+  arma_rng::set_seed(0);
+
+  double eta = strtod(argv[1], NULL);
+  bool use_stupid_solver = strtol(argv[5], NULL, 10) > 0;
+
+  mat A; colvec b; colvec z;
+  A.load(argv[2], csv_ascii);
+  b.load(argv[3], csv_ascii);
+  z.load(argv[4], csv_ascii);
 
   GLM *g = makeGLM(A, b, eta, use_stupid_solver);
-  run(g, A, b, z, lambda, eta);
+  run(g, A, b, z, eta);
 
   delete g; 
 }
